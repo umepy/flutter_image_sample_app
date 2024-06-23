1b5ae8953560cf849b008289be47a94ec5ea2601
umepy <haruya04183083@gmail.com>
2024-06-23 22:29:26 +0900

    finish to create this app

---
+++ lib/edit_snap_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'dart:typed_data';
+import 'package:image/image.dart' as image_lib;
+import 'package:flutter_image_sample_app/gen/assets.gen.dart';
+
+class ImageEditScreen extends StatefulWidget {
+  const ImageEditScreen({super.key, required this.imageBitmap});
+  final Uint8List imageBitmap;
+
+  @override
+  State<ImageEditScreen> createState() => _ImageEditScreenState();
+}
+
+class _ImageEditScreenState extends State<ImageEditScreen> {
+  late Uint8List _imageBitmap;
+  @override
+  void initState() {
+    super.initState();
+    _imageBitmap = widget.imageBitmap;
+  }
+
+  void _rotateImage(int angle) {
+    final image = image_lib.decodeImage(_imageBitmap);
+    if (image != null) {
+      final rotatedImage = image_lib.copyRotate(image, angle: angle);
+      final rotatedImageBytes = image_lib.encodeBmp(rotatedImage);
+      setState(() {
+        _imageBitmap = rotatedImageBytes;
+      });
+    }
+  }
+
+  void _flipImage() {
+    final image = image_lib.decodeImage(_imageBitmap);
+    if (image != null) {
+      final flippedImage = image_lib.flipHorizontal(image);
+      final flippedImageBytes = image_lib.encodeBmp(flippedImage);
+      setState(() {
+        _imageBitmap = flippedImageBytes;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = L10n.of(context);
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(l10n.imageEdit),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.memory(_imageBitmap),
+              IconButton(
+                  onPressed: () {
+                    _rotateImage(90);
+                  },
+                  icon: Assets.rotateIcon.svg(width: 24, height: 24)),
+              IconButton(
+                  onPressed: () {
+                    _flipImage();
+                  },
+                  icon: Assets.flipIcon.svg(width: 24, height: 24)),
+            ],
+          ),
+        ));
+  }
+}
--- lib/image_select_screen.dart
+++ lib/image_select_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as image_lib;
+import 'package:flutter_image_sample_app/edit_snap_screen.dart';
 
 class ImageSelectScreen extends StatefulWidget {
   const ImageSelectScreen({super.key});
@@ -9,9 +13,36 @@ class ImageSelectScreen extends StatefulWidget {
 }
 
 class _ImageSelectScreenState extends State<ImageSelectScreen> {
+  final ImagePicker _picker = ImagePicker();
+  Uint8List? _imageBitmap;
+
+  Future<void> _selectImage() async {
+    final XFile? imageFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+    final imageBitmap = await imageFile?.readAsBytes();
+    assert(imageBitmap != null);
+    if (imageBitmap == null) return;
+
+    final image = image_lib.decodeImage(imageBitmap);
+    assert(image != null);
+    if (image == null) return;
+
+    final image_lib.Image resizedImage;
+    if (image.width > image.height) {
+      resizedImage = image_lib.copyResize(image, width: 500);
+    } else {
+      resizedImage = image_lib.copyResize(image, height: 500);
+    }
+
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(resizedImage);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = L10n.of(context);
+    final imageBitmap = _imageBitmap;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -19,12 +50,22 @@ class _ImageSelectScreenState extends State<ImageSelectScreen> {
       ),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          if (imageBitmap != null) Image.memory(imageBitmap),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              _selectImage();
+            },
             child: Text(l10n.imageSelect),
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              if (imageBitmap != null) {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) =>
+                      ImageEditScreen(imageBitmap: imageBitmap),
+                ));
+              }
+            },
             child: Text(l10n.imageEdit),
           ),
         ]),
